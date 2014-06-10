4dfa7d599ff5cb4b3dc419eebb230336c812eb5a
Anthony Perritano <aperritano@Dr-Banner.local>
2014-06-10 15:32:01 -0500

    Initial Commit

---
+++ ios-mqtt-base/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ios-mqtt-base
+//
+//  Created by Anthony Perritano on 6/10/14.
+//  Copyright (c) 2014 LTG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
