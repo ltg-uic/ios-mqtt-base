600dc101f779f176ab82b2fc3fa24480e5e9fe6e
Anthony Perritano <aperritano@gmail.com>
2014-09-28 16:11:22 -0500

    can pick an image from the library. Need to finish upload code.

--- ios-mqtt-base/ViewController.swift
+++ ios-mqtt-base/ViewController.swift
@@ -7,22 +7,71 @@
 //
 
 import UIKit
-import Foundation
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIGestureRecognizerDelegate {
     
     @IBOutlet var topicTextField : UITextField!
     @IBOutlet var messageTextField : UITextField!
-    
+    @IBOutlet weak var posterImageView: UIImageView!
+
+    let tapRecognizer = UITapGestureRecognizer()
+
+    let uploadQueue : NSOperationQueue!
+    var url : NSURL!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view, typically from a nib.
+        //tap
+        tapRecognizer.addTarget(self, action: "selectImageFromLibrary")
+        posterImageView.addGestureRecognizer(tapRecognizer)
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func selectImageFromLibrary() {
+        var picker : UIImagePickerController = UIImagePickerController()
+        picker.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+        picker.delegate = self
+        picker.allowsEditing = false
+        self.presentViewController(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(picker: UIImagePickerController!, didFinishPickingImage image: UIImage!, editingInfo: [NSObject : AnyObject]!) {
+    
+        posterImageView.image = image
+        
+        let postURL = "http://aaa:8080/send"
+        let manager = AFHTTPRequestOperationManager()
+       // let jpegImage = UIImageJPEGRepresentation(image, 0.5)
+        
+
+//        manager.POST(postURL, parameters: nil,
+//            constructingBodyWithBlock: { (formData: (AFMultipartFormData?)->Void?) in
+//                formData.appendPartWithFileData(data: image, name: "image", fileName: "test.jpeg", mimeType: "image/jpeg")
+//            },
+//            success: { operation, response in
+//                println("[success] operation: \(operation), response: \(response)")
+//            },
+//            failure: { operation, error in
+//                println("[fail] operation: \(operation), error: \(error)")
+//            }
+//        )
+        
+
+        
+        picker.dismissViewControllerAnimated(true , completion: nil)
+    }
+    
+    @IBAction func sendImageToPoster(sender: UIButton) {
+
+
+        println("HEY THERE")
     }
 
     @IBAction func sendMessage(sender : AnyObject) {
